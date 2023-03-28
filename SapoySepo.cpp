ffbc48660dc720beee80b7b4beb0e6928c28b8c1
FrancoGonzalezL <franco.gonzalez.l@ug.uchile.cl>
2023-03-28 20:28:06 -0300

    inicio tarea1

---
+++ SapoySepo.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int sumar(vector<int>){
+    int n = vector.size()
+    for (int i=0;i<vector.size()){
+
+    }
+}
+int main(){
+    int t;
+    cin>>t;
+    vector<int> numeros(t,0);
+    while(t--){
+
+    }
+
+    return 0;
+}
