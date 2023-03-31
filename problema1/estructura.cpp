ce1c1bea521d5dfbeb17f204a66f596c8e9413bc
FrancoGonzalezL <frango.gonzalez.l@ug.uchile.cl>
2023-03-30 21:48:07 -0300

    problemda de estructuras exitoso

--- problema1/estructura.cpp
+++ problema1/estructura.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main(){
+
     ios_base::sync_with_stdio(0);cin.tie(0);  
     int N; while(cin>>N){
 
@@ -13,26 +14,28 @@ int main(){
         queue<int> queuep;
         priority_queue<int> pqueuep;
 
-        for (int i=0;i<N;i++){
+        while (N--){
             cin>>operacion>>valor;
+            auto it_stack = opciones.find("stack");
+            auto it_queue = opciones.find("queue");
+            auto it_pqueue = opciones.find("priority queue");
 
-            if(i==0 and operacion==2){
-                opciones.erase(opciones.find("stack"));
-                opciones.erase(opciones.find("queue"));
-                opciones.erase(opciones.find("priority queue"));
-                break;}
-            if (operacion==1){
-                stackp.push(valor);
-                queuep.push(valor);
-                pqueuep.push(valor);
-            }else{
-                if(opciones.find("stack")!=opciones.end() and stackp.top()!=valor)
-                {opciones.erase(opciones.find("stack"));}
-                if(opciones.find("queue")!=opciones.end() and queuep.front()!=valor)
-                {opciones.erase(opciones.find("queue"));}
-                if(opciones.find("priority queue")!=opciones.end() and pqueuep.top()!=valor)
-                {opciones.erase(opciones.find("priority queue"));}
-                stackp.pop();queuep.pop();pqueuep.pop();
+            if(operacion==1){
+                if(it_stack!=opciones.end()){stackp.push(valor);}
+                if(it_queue!=opciones.end()){queuep.push(valor);}
+                if(it_pqueue!=opciones.end()){pqueuep.push(valor);}}
+            else{
+                if(it_stack!=opciones.end()){
+                    if(stackp.size()==0 or stackp.top()!=valor){opciones.erase(it_stack);}
+                    else{stackp.pop();}}
+
+                if(it_queue!=opciones.end()){
+                    if(queuep.size()==0 or queuep.front()!=valor){opciones.erase(it_queue);}
+                    else{queuep.pop();}}
+
+                if(it_pqueue!=opciones.end()){
+                    if(pqueuep.size()==0 or pqueuep.top()!=valor){opciones.erase(it_pqueue);}
+                    else{pqueuep.pop();}}
             }}
 
         if(opciones.size()>1){cout<<"not sure"<<endl;}
@@ -41,89 +44,5 @@ int main(){
             auto result = opciones.begin();
             cout<<(*result)<<endl;}
     }
-return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-bool stack_comprobacion(vector<vector<int>> operaciones){
-    stack<int> stackprueba;
-    for(int i=0;i<operaciones.size();i++){
-        if(operaciones[i][0]==1){
-           stackprueba.push(operaciones[i][1]);
-        }else{
-            if(stackprueba.top()!=operaciones[i][1]){return false;}
-            stackprueba.pop();
-        }
-    }return true;
-}
-bool queue_comprobacion(vector<vector<int>> operaciones){
-    queue<int> queueprueba;
-    for(int i=0;i<operaciones.size();i++){
-        if(operaciones[i][0]==1){
-           queueprueba.push(operaciones[i][1]); 
-        }else{           
-            if(queueprueba.front()!=operaciones[i][1]){return false;}
-            queueprueba.pop();
-        }
-    }return true;
-}
-bool priority_comprobacion(vector<vector<int>> operaciones){
-    priority_queue<int> pqueueprueba;
-    for(int i=0;i<operaciones.size();i++){
-        if(operaciones[i][0]==1){
-           pqueueprueba.push(operaciones[i][1]); 
-        }else{           
-            if(pqueueprueba.top()!=operaciones[i][1]){return false;}
-            pqueueprueba.pop();
-        }
-    }return true;
-}
-
-int otrafuncion(){//lenta
-    ios_base::sync_with_stdio(0);cin.tie(0);
-    int N; while(cin >> N){
-
-        vector<vector<int>> operaciones(N,vector<int>(2));
-        queue<string> opciones;
-
-        for(int i=0;i<N;i++){
-            int operacion; int valor;
-            cin>>operacion>>valor;
-            operaciones[i][0]=operacion;operaciones[i][1]=valor;
-        } 
-
-        if (operaciones.size()>0 & operaciones[0][0]!=2){
-           if(stack_comprobacion(operaciones)){
-               opciones.push("stack");}
-
-           if(queue_comprobacion(operaciones)){
-            opciones.push("queue");}
-
-           if(priority_comprobacion(operaciones)){
-            opciones.push("priority queue");}
-
-           if(opciones.size()<1){cout<<"impossible"<<endl;}
-           else if(opciones.size()>1){cout<<"not sure"<<endl;}
-           else{cout<<opciones.front()<<endl;}
-        }else{cout<<"impossible"<<endl;}
-        }
 return 0;
 }
