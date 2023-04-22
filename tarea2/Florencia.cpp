384174bd162bd74705a6e86f3b1330726e0f3e21
FrancoGonzalezL <frango.gonzalez.l@ug.uchile.cl>
2023-04-22 18:33:18 -0400

    tarea2 terminada

--- tarea2/Florencia.cpp
+++ tarea2/Florencia.cpp
@@ -1,39 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);
-	int n, m; cin >> n >> m;
-	vector <vector <char>> grilla(n, vector<char>(m));
-	// ... leemos la grilla desde la entrada estándar
-    for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
-        int p;cin>>p;  
-        grilla[i][j]=p;}}
+int n, m, k, puntos = 0, casillas_con_punto = 0;
+vector <vector <char>> Grilla;
+vector<vector<int>> visited;
 
-
-
+void Recorrer_Grilla(int x, int y){
+	queue<pair<int,int>> q;
 	vector <int> dx = {1, 0, -1, 0};
 	vector <int> dy = {0, 1, 0, -1};
-	// dx y dy indican los cuatro movimientos posibles. Un vector de movimiento sería (dx[i], dy[i])
 
-	queue <pair<int,int>> q; // Nuestros nodos ahora son pares (x, y)
-	vector <vector <int>> dist(n, vector <int>(m,-1)); // ahora nuestro vector dist es una matriz de nxm
-	q.push({0,0}); // empezaremos desde el (0,0)
+	q.push({x,y});
+	visited[x][y] = 1;
+	casillas_con_punto++;
+	if(casillas_con_punto == puntos){return;}
 
 	while(!q.empty()){
-		auto [x,y] = q.front();
+		pair<int,int> coord = q.front();
+		x = coord.first, y = coord.second;
 		q.pop();
+
 		for(int dir=0; dir<4; dir++){
 			int nx = x+dx[dir];
 			int ny = y+dy[dir];
-			if( nx>=m || ny>=n ) continue;
-			if( grilla[nx][ny] != "." ) continue;
-			if(dist[nx][ny] != -1) continue; // ya fue visitado
-			dist[nx][ny] = dist[x][y]+1;
+
+			if( nx > n-1 || nx < 0 || ny < 0 || ny > m-1 ){continue;}//fuera de rango
+			if( visited[nx][ny]== 1 ) {continue;} // ya fue visitado
 			q.push({nx,ny});
+			visited[nx][ny]=1;
+			casillas_con_punto++;
+			if(casillas_con_punto==puntos){return;}
 		}
 	}
-	// la respuesta queda guardada en dist
 }
 
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+	cin >> n >> m >> k;
+	Grilla.assign(n,vector<char>(m));
+	visited.assign(n,vector<int>(m,0));
+
+    for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        char p;cin>>p;  
+        Grilla[i][j]=p;
+		if(p==35){visited[i][j]=1;}//si es # se visita
+		else{puntos++;}//se suma la cantidad de puntos en la grilla
+		}}
+	
+	puntos -= k;//A-k
+	bool break_loop = false;
+	for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        if(visited[i][j]==0){//se busca una casilla no visitada
+			Recorrer_Grilla(i,j);
+			break_loop = true;break;}
+		}if(break_loop){break;}}
+
+	for(int i=0;i<n;i++){
+	for(int j=0;j<m;j++){
+		if(visited[i][j]==1){//visitados
+			cout<<Grilla[i][j];
+		}else{
+			cout<<"X";//los que no fueron visitados se marcan con X
+		}
+		}cout<<"\n";}
+}
