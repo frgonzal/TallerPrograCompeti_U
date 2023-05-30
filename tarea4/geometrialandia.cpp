77891db09bf3f2ae500ab10c253f65848a6e5640
FrancoGonzalezL <frango.gonzalez.l@ug.uchile.cl>
2023-05-29 20:31:50 -0400

    geometrialandia terminado

--- tarea4/geometrialandia.cpp
+++ tarea4/geometrialandia.cpp
@@ -8,33 +8,31 @@ bool izq(tuple<ll,ll,ll> tupla , pair<ll,ll> punto){
     ll a,b,c;
     auto [x,y] = punto;
     tie(a,b,c) = tupla;
-    if(b==0){
-        if()
+    // ax+by+c == 0
+    // by = -ax-c
+    if(!b){//b==0
+        return (-c < a*x);
     }else{
-
+        return (-a*x - c < b*y);
     }
-
 }
 
 vector<tuple<ll,ll,ll>> rectas;
 int main(){
-    int x,y;cin>>x>>y;
-    pair<ll,ll> casa = {x,y}
-    cin>>x>>y;
-    pair<ll,ll> fcgm = {x,y}
-    int n;cin>>n;
+    ll x,y,a,b,c;
+    cin>>x>>y; pair<ll,ll> casa = {x,y};
+    cin>>x>>y; pair<ll,ll> fcgm = {x,y};
+    int n; cin>>n;
     for(int i=0;i<n;i++){
-        int a,b,c;cin>>a>>b>>c;
+        cin>>a>>b>>c;
         rectas.push_back({a,b,c});}
 
     int contador = 0;
     for(auto recta: rectas){
-        if((izq(recta,casa) and !izq(recta,fcgm)) || (!izq(recta,casa) and izq(recta,fcgm))){
-
-        } 
+        if(    ( izq(recta,casa) and !izq(recta,fcgm)) 
+            || (!izq(recta,casa) and  izq(recta,fcgm)) ){
+                contador++;} 
     }
-
-
-
+    cout<<contador<<"\n";
     return 0;
 }
