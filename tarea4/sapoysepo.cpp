145e791cf0f08bbd3d8a1babdada8479375f9b3b
FrancoGonzalezL <frango.gonzalez.l@ug.uchile.cl>
2023-05-29 21:39:39 -0400

    sapoysepo...

--- tarea4/sapoysepo.cpp
+++ tarea4/sapoysepo.cpp
@@ -1,119 +1,109 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-struct point{
-    ll x,y;
-    point(ll x=0, ll y=0):x(x), y(y){}
+struct pt {
+    double x, y;
 
-    point operator+(point p){ return {x + p.x, y + p.y};}
-    point operator-(point p){ return {x - p.x, y - p.y};}
-
-    ll operator*(point p){return  x*p.x + y*p.y;}
-    ll operator^(point p){return  x*p.y - y*p.x;}
-
-    double dist(){return sqrt(x*x+y*y);}
-    ll dist2(){return        x*x+y*y;}
-
-    bool operator<(point p){return x < p.x;}
-    //bool operator>(point p){return x > p.x;}
-    //bool operator==(point p){return x==p.x;}
-    bool leftOF(point A, point B){
-        return ((B-A)^(*this-A)) >= 0;}
-    bool IN(point A, point B){
-        return ((B-A)^(*this-A)) == 0;}
-    void print(){
-        cout<<"("<<x<<","<<y<<")"<<endl;
-    }
+    bool operator==(pt p){
+        return x==p.x and y==p.y;}
+    double dist(pt p){
+        return sqrt((x-p.x)*(x-p.x) + (y-p.y)*(y-p.y));}
+    
 };
-vector<point> figura;
-vector<point> izq;
-vector<point> der;
 
-int n;
-point sapo;
-point sepo;
-
-bool entre(point a, point c, point b){
-    return (( a<c && c<b ) || ( b<c && c<a ));
+int orientation(pt a, pt b, pt c) {
+    double v = a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y);
+    if (v < 0) return -1; // clockwise
+    if (v > 0) return +1; // counter-clockwise
+    return 0;
 }
-
-double camino(bool left, vector<point> &puntos){
-    stack<point> pila;
-    bool leftof;
-    if(puntos.size()>0){
-        pila.push(puntos[0]);
-    for(int i=1;i<puntos.size();i++){ 
-        auto punto = pila.top();pila.pop();
-        if(puntos[i].leftOF(sapo,punto) == left){
-            pila.push(puntos[i]);
-        }else{
-            pila.push(punto);
-            for(int j=i;j<puntos.size();j++){
-                auto punto = pila.top();pila.pop();
-                if( puntos[j].leftOF(punto,sepo) == left){
-                    pila.push(punto);
-                    pila.push(puntos[j]);
-                }else{//si esta adentro de la linea que forman el punto y sepo
-                      //entonces se puede llegar directo
-                    pila.push(punto);
-                    break;
-                }
-            }
-            break;
-        }}}
-
-    if(pila.empty()){return (sapo-sepo).dist();}
-
-    double dist = 0;    
-    auto punto1 = pila.top();pila.pop();
-
-    dist += (sepo - punto1).dist();
-
-    //punto1.print();
-    while(!pila.empty()){
-        auto punto2 = pila.top();pila.pop();
-        //punto2.print();
-        dist += (punto1-punto2).dist();
-        punto1 = punto2;
+bool cw(pt a, pt b, pt c, bool include_collinear) {
+    int o = orientation(a, b, c);
+    return o < 0 || (include_collinear && o == 0);
+}
+bool collinear(pt a, pt b, pt c) { return orientation(a, b, c) == 0; }
+void convex_hull(vector<pt>& a, bool include_collinear = false) {
+    pt p0 = *min_element(a.begin(), a.end(), [](pt a, pt b) {
+        return make_pair(a.y, a.x) < make_pair(b.y, b.x);
+    });
+    sort(a.begin(), a.end(), [&p0](const pt& a, const pt& b) {
+        int o = orientation(p0, a, b);
+        if (o == 0)
+            return (p0.x-a.x)*(p0.x-a.x) + (p0.y-a.y)*(p0.y-a.y)
+                < (p0.x-b.x)*(p0.x-b.x) + (p0.y-b.y)*(p0.y-b.y);
+        return o < 0;
+    });
+    //que hace esto??
+    if (include_collinear) {
+        int i = (int)a.size()-1;
+        while (i >= 0 && collinear(p0, a[i], a.back())) i--;
+        reverse(a.begin()+i+1, a.end());
     }
-    dist += (sapo-punto1).dist();
-    return dist;
+
+    vector<pt> st;
+    for (int i = 0; i < (int)a.size(); i++) {
+        while (st.size() > 1 && !cw(st[st.size()-2], st.back(), a[i], include_collinear))
+            st.pop_back();
+        st.push_back(a[i]);
+    }
+
+    a = st;
 }
+
+vector<pt> puntos;
 
 int main(){
     ios_base::sync_with_stdio(0);cin.tie(0);
-    cin>>n; ll x,y;
-    //leer puntos
+    int n; cin>>n;
+    double x,y;
     for(int i=0;i<n;i++){
         cin>>x>>y;
-        figura.push_back({x,y});}
+        puntos.push_back({x,y});}
 
-    //leer sapo y sexo
-    cin>>x>>y; sapo = {x,y};
-    cin>>x>>y; sepo = {x,y};
+    cin>>x>>y;
+    pt sapo = {x,y};
+    puntos.push_back({x,y});
+    cin>>x>>y;
+    pt sepo = {x,y};
+    puntos.push_back({x,y});
 
-    //si sapo y sepo estan ambos a la izq de alguna arista entonces se puede llegar directo
-    point punto1 = figura[0];
-    for(int i=1;i<n;i++){
-        if(sapo.leftOF(figura[i],punto1) and sepo.leftOF(figura[i],punto1)){
-            cout<<(sapo-sepo).dist()<<endl; return 0;}
-        punto1 = figura[i];}
-        if(sapo.leftOF(punto1,figura[0]) and sepo.leftOF(punto1,figura[0])){
-            cout<<(sapo-sepo).dist()<<endl; return 0;}
-    //si pasamos esto aseguramos que no existe camino directo
+    convex_hull(puntos, true);
 
+    //necesariamente uno debe estar en el convex hull? si no fuera asi
+    //tendrian que estar los 2 contenidos en la figura
+    int sapoysepo=0;
+    for(pt punto: puntos){
+        if(punto==sapo){sapoysepo++;}
+        if(punto==sepo){sapoysepo++;}}
+    if(sapoysepo==1){//uno no esta en el convex hull
+    cout<<fixed<<setprecision(15)<<sapo.dist(sepo)<<"\n";return 0;
+    }
 
-    //ordenar puntos segun posicion ejex
-    sort(figura.begin(), figura.end());
-    //for(int i=0;i<n;i++){figura[i].print();}
-    //comprobar si estan arriba o abajo
-    for(int i=0;i<n;i++){
-        if(figura[i].leftOF(sapo,sepo) and !figura[i].IN(sapo,sepo)){
-            izq.push_back(figura[i]);
-        }else if(!figura[i].leftOF(sapo,sepo)){
-            der.push_back(figura[i]);}} 
-    double minimo = min(camino(true,izq), camino(false, der));
-    cout<<fixed<< setprecision(15) <<minimo<< "\n";
+    double camino1 = 0;
+    double camino2 = 0;
+
+    bool camino = true;//si estamos en el camino de ida o vuelta
+    pt punto1 = puntos[0];
+    pt punto2;
+    for(int i=1;i<n+2;i++){
+        punto2 = puntos[i];
+
+        if(punto1==sapo || punto1==sepo){
+            camino=!camino;}
+
+        if(camino){
+            camino1+=punto1.dist(punto2);
+        }else{
+            camino2+=punto1.dist(punto2);}
+
+        punto1=punto2;
+    }
+    if(puntos[n-1]==sapo || puntos[n-1]==sepo){camino=!camino;}
+    if(camino){
+        camino1+=puntos[n-1].dist(puntos[0]);
+    }else{
+        camino2+=puntos[n-1].dist(puntos[0]);}
+
+    cout<<fixed<< setprecision(15) <<min(camino1,camino2)<< "\n";
     return 0;
 }
