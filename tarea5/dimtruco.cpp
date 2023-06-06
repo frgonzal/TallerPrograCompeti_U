826f14701a8a1e4f0739efa674e460cf5b9cc81a
FrancoGonzalezL <franco.gonzalez.l@ug.uchile.cl>
2023-06-06 16:59:30 -0400

    dimtruco aun sin funcionar

--- tarea5/dimtruco.cpp
+++ tarea5/dimtruco.cpp
@@ -10,18 +10,21 @@ void Primos(){
     vector<int> Criba(max_N+1, 0);// O(nlogn)
     for(int i=2; i*i<=max_N+1; i++){
         for(int j=i*i; j<max_N+1; j+=i){
-            Criba[j]=1;
-            if(j*j>K)ma_k++;
-            else mi_k++;
+            if(!Criba[j]){
+                if(j>ks)ma_k++;
+                else mi_k++;
+                Criba[j]=1;
+            }
         }
     }
     //ks el numero que divide la Criba
     // ma_k los menores a k
     // mi_k los mayores a k
-    // max_N + 1 - (mi_k + ma_k + 2) == primos totales //el 0 y el 1 no son contados originalmente
-    // (max_N - Ks) - ma_k == primos totales mayores a k
+    // max_N - (mi_k + ma_k + 1) == primos totales //el 1 no se cuenta
+    // ks - (mi_k + 1)  = primos menores que k
+    // primos totales - primos menores que k = primos mayores que k
     cuadrados_perfectos = max_N - (mi_k + ma_k + 1);
-    mayores_K = (max_N - ks) - ma_k;
+    mayores_K = (cuadrados_perfectos) - (ks - mi_k - 1);
 }    
 
 int main(){
@@ -30,7 +33,6 @@ int main(){
     //Numero de primos cuyos cuadrados son menores que N
     cin>>N>>K;
     //primo maximo == (ll) sqrt(N)
-
     max_N = (ll) sqrt(N);
     Primos();
     cout<<cuadrados_perfectos<<" ";
