2bdc497e66f3e6ce944cbcdba71d7e9ae891eb44
FrancoGonzalezL <franco.gonzalez.l@ug.uchile.cl>
2023-09-16 16:48:23 -0300

    (Binary Search) Problema A

--- Tarea2B/A.cpp
+++ Tarea2B/A.cpp
@@ -1,34 +1,35 @@
 #include <bits/stdc++.h>
 typedef long long ll;
 int n;
-ll t, k, maxElem, mid;
+ll t, k, maxElem = 0, mid;
 std::vector <ll> K;
 
 int sumT(ll tiempo){
     ll paquetes = t;
-    for(int i=0; i<n; i++){
-        paquetes -= tiempo / K[i]; // paquetes que puedo hacer en tiempo t con maquina K[i];
+    ll total = 0;
+    for(int i=0; i<n && total < paquetes; i++){
+        total += (tiempo / K[i]); // paquetes que puedo hacer en el tiempo con maquina K[i];
     }
-    if (paquetes > 0) return 0;//no se puede hacer en el tiempo;
-    else return 1;
+    if (total < paquetes) return 0;      //no se pueden hacer todos los paquetes en el tiempo;
+    return 1;
 }
 
 int main(){
     std::ios_base::sync_with_stdio(0);std::cin.tie(0);
     std::cin>>n>>t;
-
+    K.assign(n, 0);
+    
     for(int i=0;i<n;i++){
         std::cin>>k;
-        K.push_back(k);
+        K[i] = k;
         maxElem = std::max(maxElem, k);
     }
     
     ll l = 0, r = maxElem*t;
     while(l < r){
-        mid = (l+r)/2;
-        if( sumT(mid) ) r = mid; // Si se puede hacer en ese tiempo (mid) entonces...
+        mid = l + (r-l)/2;
+        if( sumT(mid) ) r = mid; // Si se puede hacer en ese tiempo (mid).
         else l = mid+1;
     }
-
     std::cout<<r<<"\n";
 }
